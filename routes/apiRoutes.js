fd43968c752bb24c2fc3b3c257ef317e4671462b
Kait <kweishaar@hawk.iit.edu>
2021-08-07 11:00:04 -0500

    server done

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,2 +1,24 @@
 const router = require("express").Router();
 const { notes } = require("../../db/db");
+
+router.get("/notes", (req, res) => {
+    let results = notes;
+    res.json(results);
+});
+
+router.post("/notes", (req, res) => {
+    req.body.id = notes.length.toString();
+    const newNote = req.body;
+    let notesData = JSON.parse(fs.readFileSync("./db/db.json", "utf8"));
+    notesData.push(newNote);
+    fs.writeFileSync('./db./db.json', JSON.stringify(notesData));
+    res.json(notesData);
+})
+
+router.delete("/api/notes/:id", (req, res) => {
+    let noteID = req.params.id.toString();
+    let noteData = JSON.parse(fs.readFileSync("./db/db.json", "utf8"));
+    const newData = noteData.filter(note => note.id.toString() !== noteID);
+    fs.writeFileSync('./db./db.json', JSON.stringify(newData));
+    res.json(newData);
+})
--- server.js
+++ server.js
@@ -4,3 +4,12 @@ const path = require('path');
 const PORT = process.env.PORT || 3001;
 app.use(express.urlencoded({ extended: true}));
 app.use(express.json());
+app.use(express.static("public"));
+
+require('./routes/apiRoutes')(router);
+require('./routes/htmlRoutes')(router);
+
+
+app.listen(PORT, function() {
+    console.log(`App listening on PORT: ${PORT}`);
+});
