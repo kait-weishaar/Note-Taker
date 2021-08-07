e9f35c2a55af877e37eb22cf0ee28c27ce2b7d0c
Kait <kweishaar@hawk.iit.edu>
2021-08-07 11:06:07 -0500

    re-added app

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const fs = require('fs');
 const path = require('path');
+const app = express();
 const PORT = process.env.PORT || 3001;
 app.use(express.urlencoded({ extended: true}));
 app.use(express.json());
